ed27b18e05762bca3b7ba865546814191064d73e
Henry Bui <viethuongdev@gmail.com>
2018-09-15 15:30:40 +0700

    Random username for old users data

--- database/migrations/2018_09_15_043450_add_fields_to_users_table.php
+++ database/migrations/2018_09_15_043450_add_fields_to_users_table.php
@@ -14,8 +14,16 @@ class AddFieldsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->nullable()->before('email');
+            $table->string('username')->before('email');
         });
+
+        // Update old accounts (15/09/2018).
+        $users = \App\User::all();
+        foreach ($users as $user) {
+            $randomUserName = \App\User::generateUniqueUsername($user->getCompleteName());
+            $user->username = $randomUserName;
+            $user->save();
+        }
     }
 
     /**
