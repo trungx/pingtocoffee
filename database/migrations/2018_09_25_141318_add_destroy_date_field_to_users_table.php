e0f27e694b05acaa2f9d9e2d606284298481c564
Bùi Viết Hướng (Henry) <viethuongdev@gmail.com>
2018-09-26 22:25:08 +0700

    Update migration for refresh command

--- database/migrations/2018_09_25_141318_add_destroy_date_field_to_users_table.php
+++ database/migrations/2018_09_25_141318_add_destroy_date_field_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsToUsersTable extends Migration
+class AddDestroyDateFieldToUsersTable extends Migration
 {
     /**
      * Run the migrations.
