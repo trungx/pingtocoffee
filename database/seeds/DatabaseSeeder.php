f003118dd588270ca396e89dacd7de9182e7c069
Bùi Viết Hướng (Henry) <viethuongdev@gmail.com>
2018-10-06 11:50:53 +0700

    Change the seeding file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,37 +11,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /**
-         * Seeder for local environment.
-         */
-        if (env('APP_ENV') == 'local') {
-            // Fake 2 users easy remember information
-            $this->call(FakeUserTableSeeder::class);
-            $this->call(UserTableSeeder::class);
-            $this->call(RelationshipTableSeeder::class);
-            $this->call(ContactLogTableSeeder::class);
-            $this->call(ReminderTableSeeder::class);
-            $this->call(ContactFieldValueTableSeeder::class);
-        }
-
-        /**
-         * Seeder for testing environment.
-         */
-        if (env('APP_ENV') == 'testing') {
-            // Fake 2 users easy remember information
-            $this->call(FakeUserTableSeeder::class);
-            $this->call(UserTableSeeder::class);
-            $this->call(RelationshipTableSeeder::class);
-            $this->call(ContactLogTableSeeder::class);
-            $this->call(ReminderTableSeeder::class);
-            $this->call(ContactFieldValueTableSeeder::class);
-        }
-
-        /**
-         * Seeder for production environment.
-         */
-        if (env('APP_ENV') == 'production') {
-            // Do something
+        switch (\Illuminate\Support\Facades\App::environment()) {
+            case 'local':
+                $this->call(FakeUserTableSeeder::class);
+                $this->call(UserTableSeeder::class);
+                $this->call(RelationshipTableSeeder::class);
+                $this->call(ContactLogTableSeeder::class);
+                $this->call(ReminderTableSeeder::class);
+                $this->call(ContactFieldValueTableSeeder::class);
+                break;
+            case 'testing':
+                $this->call(FakeUserTableSeeder::class);
+                break;
+            case 'production':
+                break;
+            default:
+                break;
         }
     }
 }
