dbed9e406da7b4424430c2aa10780fe7c1af8ae2
Bùi Viết Hướng (Henry) <viethuongdev@gmail.com>
2018-10-06 11:34:35 +0700

    Change unit test configuration

--- config/database.php
+++ config/database.php
@@ -78,6 +78,20 @@ return [
             'prefix' => '',
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TEST_HOST', '127.0.0.1'),
+            'port' => env('DB_TEST_PORT', '3306'),
+            'database' => env('DB_TEST_DATABASE'),
+            'username' => env('DB_TEST_USERNAME'),
+            'password' => env('DB_TEST_PASSWORD'),
+            'unix_socket' => env('DB_TEST_SOCKET', ''),
+            'charset' => env('DB_USE_UTF8MB4', true) ? 'utf8mb4' : 'utf8',
+            'collation' => env('DB_USE_UTF8MB4', true) ? 'utf8mb4_unicode_ci' : 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
+
     ],
 
     /*
