f07d459a4c01d9d0533ac55f03a2256adc4bc60f
Henry Bui <viethuongdev@gmail.com>
2018-11-13 00:39:35 +0700

    Change activity logs ui

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -5,12 +5,12 @@ return [
     'feed_heading' => 'Activity Log',
     'signed_up_event' => 'Hooray! You signed up and become a member of ' . config('app.name'),
     'first_reminder_event' => 'You created the first reminder!',
-    'add_note_event_body' => 'You added a note for <a href="/:username?tab=notes" class="link-gray-dark b">:fullName</a>',
-    'delete_note_event_body' => 'You deleted a note of <a href="/:username?tab=notes" class="link-gray-dark b">:fullName</a>',
-    'add_contact_log_event_body' => 'You added a contact log for <a href="/:username?tab=contact-logs" class="link-gray-dark b">:fullName</a>',
-    'add_reminder_event_body' => 'You set new reminder for <a href="/:username?tab=reminders" class="link-gray-dark b">:fullName</a>',
-    'edit_reminder_event_body' => 'You edit reminder of <a href="/:username" class="link-gray-dark b">:fullName</a>',
-    'add_user_event_body' => 'You and <a href="/:username" class="link-gray-dark b">:fullName</a> has been friends!',
+    'add_note_event_body' => 'You added a note for <a href="/:username?tab=notes">:fullName</a>',
+    'delete_note_event_body' => 'You deleted a note of <a href="/:username?tab=notes">:fullName</a>',
+    'add_contact_log_event_body' => 'You added a contact log for <a href="/:username?tab=contact-logs">:fullName</a>',
+    'add_reminder_event_body' => 'You set new reminder for <a href="/:username?tab=reminders">:fullName</a>',
+    'edit_reminder_event_body' => 'You edit reminder of <a href="/:username">:fullName</a>',
+    'add_user_event_body' => 'You and <a href="/:username">:fullName</a> has been friends!',
 
     // Default event icons
     'signed_up_icon_class' => 'fas fa-trophy',
